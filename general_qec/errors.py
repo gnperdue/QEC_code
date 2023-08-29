614f177136c85509f3acc3fb5e019165e0c94b1c
Gabriel Perdue <gnperdue@hey.com>
2023-08-29 11:39:51 -0500

    work PEP8 style enforcement

--- general_qec/errors.py
+++ general_qec/errors.py
@@ -4,7 +4,10 @@ import numpy as np
 import random
 from general_qec.qec_helpers import *
 from general_qec.gates import *
+#from general_qec.gates import sigma_x, sigma_y, sigma_z, sigma_I
 
+# TODO: make a `random_qubit_gate_error()` that accepts a unitary as an
+# argument and use it to refactor `random_qubit_x_error`, z error, etc.
 
 def random_qubit_x_error(logical_state, qubit_range=None):
     """
@@ -13,9 +16,9 @@ def random_qubit_x_error(logical_state, qubit_range=None):
     probability as any given qubit.
 
     * logical_state: The logical state of the N qubit system you wish to
-    apply the error to
+    apply the error to - e.g. np.kron(one, zero)
     * qubit_range: The indices you want to consider in your error application
-    (starts at 0)
+    (starts at 0) - e.g., a *tuple* like (0, 2)
     """
     # total number of qubits in your system
     nqubits = int(np.log(len(logical_state))/np.log(2))
@@ -39,58 +42,72 @@ def random_qubit_x_error(logical_state, qubit_range=None):
     return errored_logical_state, error_index
 
 
-### Applies a random Z rotation to one of the physical qubits in your system (randomly) ### 
-def random_qubit_z_error(logical_state, qubit_range = None):
-    # logical_state: The logical state of the three qubit system you wish to apply the error to
-    # qubit_range: The indices you want to consider in your error application (starts at 0)
-   
+def random_qubit_z_error(logical_state, qubit_range=None):
+    """
+    Applies a random Z rotation to one of the physical qubits in your system,
+    selected uniformly randomly - can choose "no error" with the same
+    probability as any given qubit.
+
+    * logical_state: The logical state of the N qubit system you wish to
+    apply the error to - e.g. np.kron(one, zero)
+    * qubit_range: The indices you want to consider in your error application
+    (starts at 0) - e.g., a *tuple* like (0, 2)
+    """
     # total number of qubits in your system
-    n = int(np.log(len(logical_state))/np.log(2))
-    
+    nqubits = int(np.log(len(logical_state))/np.log(2))
+
     # Choose the index of the qubit you want to apply the error to.
-    error_index = random.randint(-1,n-1)
-    if qubit_range != None:
-        if error_index != -1:
-            error_index = random.randint(qubit_range[0], qubit_range[1])
-             
+    error_index = -1
+    if qubit_range is not None:
+        error_index = random.randint(qubit_range[0], qubit_range[1])
+    else:
+        error_index = random.randint(-1, nqubits-1)
+
     # Apply the error depending on the index
-    if error_index == -1:
-        errored_logical_state = logical_state
-    else:   
-        error_gate = np.kron(np.identity(2**(error_index)), np.kron(sigma_z, np.identity(2**(n-error_index-1))))
+    errored_logical_state = logical_state
+    if error_index > -1:
+        error_gate = np.kron(
+            np.identity(2**(error_index)),
+            np.kron(sigma_z, np.identity(2**(nqubits-error_index-1)))
+        )
         errored_logical_state = np.dot(error_gate, logical_state)
 
     return errored_logical_state, error_index
 
 
-### - - - - - - - - - - Gates which contain probability for errors (line connectivity) - - - - - - - - - - ###
+### - Gates which contain probability for errors (line connectivity) - ###
 
-### Takes the density matrix after a perfect operation and applies an error gate based on probability of error ###
+
 def gate_error(rho, error_prob, index, n):
-    # rho: density matrix of qubit system after perfect gate was applied
-    # error_prob: probability for gate operation error
-    # index: index of qubit that gate was applied (target qubit in this case)
-    # n: total number of qubits in your system
-    
+    """
+    Take a density matrix after a perfect operation and apply an error gate
+    based on probability of an error.
+
+    * rho: density matrix of qubit system after perfect gate was applied
+    * error_prob: probability for gate operation error
+    * index: index of qubit that gate was applied (target qubit in this case)
+    * n: total number of qubits in your system
+    """
     # qubit error rates:
-    KD0 = np.sqrt(1-error_prob) * sigma_I
-    KD1 = np.sqrt(error_prob/3) * sigma_x
-    KD2 = np.sqrt(error_prob/3) * sigma_z
-    KD3 = np.sqrt(error_prob/3) * sigma_y
-    
+    KD0 = np.sqrt(1-error_prob) * sigma_I # pylint: disable=invalid-name
+    KD1 = np.sqrt(error_prob/3) * sigma_x # pylint: disable=invalid-name
+    KD2 = np.sqrt(error_prob/3) * sigma_z # pylint: disable=invalid-name
+    KD3 = np.sqrt(error_prob/3) * sigma_y # pylint: disable=invalid-name
+
     # qubit error gates
-    KD0 = np.kron(np.identity(2**(index)), np.kron(KD0, np.identity(2**(n-index-1))))
-    KD1 = np.kron(np.identity(2**(index)), np.kron(KD1, np.identity(2**(n-index-1)))) 
-    KD2 = np.kron(np.identity(2**(index)), np.kron(KD2, np.identity(2**(n-index-1)))) 
-    KD3 = np.kron(np.identity(2**(index)), np.kron(KD3, np.identity(2**(n-index-1))))
-    
-    # apply error gates (qubit 0 and qubit 2 will not be affected by error gates, although we do apply Identity to q0)
-    D_rho = np.dot(KD0, np.dot(rho, KD0.conj().T)) + np.dot(
-        KD1, np.dot(rho, KD1.conj().T)) + np.dot(
-        KD2, np.dot(rho, KD2.conj().T)) + np.dot(
-        KD3, np.dot(rho, KD3.conj().T))
-    
-    return D_rho
+    KD0 = np.kron(np.identity(2**(index)), np.kron(KD0, np.identity(2**(n-index-1)))) # pylint: disable=invalid-name
+    KD1 = np.kron(np.identity(2**(index)), np.kron(KD1, np.identity(2**(n-index-1)))) # pylint: disable=invalid-name
+    KD2 = np.kron(np.identity(2**(index)), np.kron(KD2, np.identity(2**(n-index-1)))) # pylint: disable=invalid-name
+    KD3 = np.kron(np.identity(2**(index)), np.kron(KD3, np.identity(2**(n-index-1)))) # pylint: disable=invalid-name
+
+    # apply error gates
+    d_rho = \
+        np.dot(KD0, np.dot(rho, KD0.conj().T)) + \
+        np.dot(KD1, np.dot(rho, KD1.conj().T)) + \
+        np.dot(KD2, np.dot(rho, KD2.conj().T)) + \
+        np.dot(KD3, np.dot(rho, KD3.conj().T))
+
+    return d_rho
 
 
 ### - - - CNOT Gates - - - ###
