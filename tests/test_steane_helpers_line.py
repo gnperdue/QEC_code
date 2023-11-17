2111c2ad186e56eef05f38a580649720efb3de9f
Gabriel Perdue <gnperdue@hey.com>
2023-11-17 09:54:08 -0600

    fixing up steane tests

---
+++ tests/test_steane_helpers_line.py
@@ -0,0 +1,93 @@
+'''
+Usage:
+    python test_steane_helpers.py
+'''
+import unittest
+import random
+import logging
+import sys
+import numpy as np
+from general_qec.qec_helpers import one, zero, superpos
+from general_qec.qec_helpers import ancilla_reset
+from general_qec.qec_helpers import print_state_info
+from general_qec.qec_helpers import vector_state_to_bit_state
+from general_qec.gates import sigma_I, sigma_x, sigma_y, sigma_z
+# from circuit_specific.steane_helpers import \  # TODO use these later...
+#     k_one, k_two, k_three, \
+#     k_four, k_five, k_six
+from circuit_specific.steane_helpers import bit_flip_error
+from circuit_specific.steane_helpers import initialize_larger_steane_code
+from circuit_specific.steane_helpers import initialize_steane_logical_state
+from circuit_specific.steane_helpers import phase_flip_error
+from circuit_specific.steane_helpers import simultaneous_steane_code
+from circuit_specific.steane_helpers import steane_phase_correction
+from circuit_specific.steane_helpers import steane_bit_correction
+from circuit_specific.steane_helpers import steane_dataq_logical_zero
+from circuit_specific.steane_helpers import steane_dataq_logical_one
+from circuit_specific.steane_helpers import steane_dataq_logical_superpos
+from circuit_specific.steane_helpers import initialize_steane_line_conn
+from circuit_specific.steane_helpers import steane_line_conn_phase_correction
+from circuit_specific.steane_helpers import steane_line_conn_bit_correction
+
+LOGGER = logging.getLogger(__name__)
+
+ANCILLA_3ZERO = \
+    np.kron(zero,
+    np.kron(zero, zero)
+)
+
+ZERO_STATE7Q = \
+    np.kron(zero,
+    np.kron(zero,
+    np.kron(zero,
+    np.kron(zero,
+    np.kron(zero,
+    np.kron(zero, zero)
+)))))
+ONE_STATE7Q = \
+    np.kron(one,
+    np.kron(one,
+    np.kron(one,
+    np.kron(one,
+    np.kron(one,
+    np.kron(one, one)
+)))))
+SUPERPOS_STATE7Q = (1. / np.sqrt(2.0)) * (ZERO_STATE7Q + ONE_STATE7Q)
+
+
+class TestSteaneLineConnectivityCode(unittest.TestCase):
+    """Tests for Steane code functions for the 7+3 qubit system with line connectivity."""
+
+    def setUp(self) -> None:
+        self.n_qubits = 7  # number of data qubits in our system
+        self.n_ancilla = 3 # for "simultaneous" Steane
+        self.n_qtotal = self.n_qubits + self.n_ancilla
+
+    def test_line_phase_and_bit_flip_error_correction_zero_state(self):
+        """
+        Test Line Connected Steane initialization, phase, and bit flip error corrections for logical zero
+        """
+        LOGGER.info(sys._getframe().f_code.co_name) # pylint: disable=protected-access
+        random.seed(11)
+        # -
+        initialized_zero_state = initialize_steane_line_conn(ZERO_STATE7Q)
+        initialized_zero_state = ancilla_reset(initialized_zero_state, self.n_ancilla)
+        self.assertTrue(
+            np.allclose(initialized_zero_state, np.kron(steane_dataq_logical_zero(), ANCILLA_3ZERO))
+        )
+        # try 5 random phase flips (equal chance of any or no qubits)
+        for _ in range(5):
+            phase_error_state = phase_flip_error(initialized_zero_state, self.n_qtotal)[0]
+            corrected_state = steane_line_conn_phase_correction(phase_error_state)
+            corrected_state = ancilla_reset(corrected_state, self.n_ancilla)
+            self.assertTrue(np.allclose(initialized_zero_state, corrected_state))
+        # try 5 random bit flips (equal chance of any or no qubits)
+        for _ in range(5):
+            bit_error_state = bit_flip_error(initialized_zero_state, self.n_qtotal)[0]
+            corrected_state = steane_line_conn_bit_correction(bit_error_state)
+            corrected_state = ancilla_reset(corrected_state, self.n_ancilla)
+            self.assertTrue(np.allclose(initialized_zero_state, corrected_state))
+
+
+if __name__ == '__main__':
+    unittest.main()
