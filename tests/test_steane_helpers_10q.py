1ac871a86689271c10e9bde715ac821232c5bde8
Gabriel Perdue <gnperdue@hey.com>
2023-11-17 09:46:12 -0600

    remove 13q and line connectivity tests

--- tests/test_steane_helpers_10q.py
+++ tests/test_steane_helpers_10q.py
@@ -35,13 +35,6 @@ ANCILLA_3ZERO = \
     np.kron(zero,
     np.kron(zero, zero)
 )
-ANCILLA_6ZERO = \
-    np.kron(zero,
-    np.kron(zero,
-    np.kron(zero,
-    np.kron(zero,
-    np.kron(zero, zero)
-))))
 
 ZERO_STATE7Q = \
     np.kron(zero,
@@ -209,85 +202,6 @@ class Test10QSteaneCode(unittest.TestCase):
             corrected_state = steane_bit_correction(phase_error_state)
             corrected_state = ancilla_reset(corrected_state, self.n_ancilla)
             self.assertTrue(np.allclose(initialized_superpos_state, corrected_state))
-
-
-class Test13QSteaneCode(unittest.TestCase):
-    """Tests for Steane code functions for the 7+6 qubit system."""
-
-    def test_13q_phase_and_bit_flip_error_correction(self):
-        """Test 13-qubit Steane initialization, phase, and bit flip error corrections"""
-        LOGGER.info(sys._getframe().f_code.co_name) # pylint: disable=protected-access
-        n_qubits = 7  # number of data qubits in our system
-        n_ancilla = 6 # for "simultaneous" Steane
-        n_qtotal = n_qubits + n_ancilla
-        # -
-        random.seed(10)
-        initialized_zero_state = initialize_larger_steane_code(ZERO_STATE7Q)
-        self.assertEqual(initialized_zero_state.shape, (2**n_qtotal,))
-        initialized_zero_state = ancilla_reset(initialized_zero_state, n_ancilla)
-        self.assertTrue(
-            np.allclose(initialized_zero_state, np.kron(steane_dataq_logical_zero(), ANCILLA_6ZERO))
-        )
-        # too slow for multiple tests - phase flip
-        phase_error_state, error_index = phase_flip_error(initialized_zero_state, n_qtotal)
-        self.assertEqual(error_index, 5)
-        corrected_state = simultaneous_steane_code(phase_error_state)
-        corrected_state = ancilla_reset(corrected_state, n_ancilla)
-        self.assertTrue(
-            np.allclose(initialized_zero_state, corrected_state) or
-            np.allclose(initialized_zero_state, -1.0 * corrected_state)
-        )
-        # -
-        random.seed(11)
-        initialized_one_state = initialize_larger_steane_code(ONE_STATE7Q)
-        self.assertEqual(initialized_one_state.shape, (2**n_qtotal,))
-        initialized_one_state = ancilla_reset(initialized_one_state, n_ancilla)
-        self.assertTrue(
-            np.allclose(initialized_one_state, np.kron(steane_dataq_logical_one(), ANCILLA_6ZERO))
-        )
-       # too slow for multiple tests - bit flip
-        bit_error_state, error_index = bit_flip_error(initialized_one_state, n_qtotal)
-        self.assertEqual(error_index, 6)
-        corrected_state = simultaneous_steane_code(bit_error_state)
-        corrected_state = ancilla_reset(corrected_state, n_ancilla)
-        self.assertTrue(
-            np.allclose(initialized_one_state, corrected_state) or
-            np.allclose(initialized_one_state, -1.0 * corrected_state)
-        )
-
-
-class TestSteaneLineConnectivityCode(unittest.TestCase):
-    """Tests for Steane code functions for the 7+3 qubit system with line connectivity."""
-
-    def setUp(self) -> None:
-        self.n_qubits = 7  # number of data qubits in our system
-        self.n_ancilla = 3 # for "simultaneous" Steane
-        self.n_qtotal = self.n_qubits + self.n_ancilla
-
-    def test_line_phase_and_bit_flip_error_correction_zero_state(self):
-        """
-        Test Line Connected Steane initialization, phase, and bit flip error corrections for logical zero
-        """
-        LOGGER.info(sys._getframe().f_code.co_name) # pylint: disable=protected-access
-        random.seed(11)
-        # -
-        initialized_zero_state = initialize_steane_line_conn(ZERO_STATE7Q)
-        initialized_zero_state = ancilla_reset(initialized_zero_state, self.n_ancilla)
-        self.assertTrue(
-            np.allclose(initialized_zero_state, np.kron(steane_dataq_logical_zero(), ANCILLA_3ZERO))
-        )
-        # try 5 random phase flips (equal chance of any or no qubits)
-        for _ in range(5):
-            phase_error_state = phase_flip_error(initialized_zero_state, self.n_qtotal)[0]
-            corrected_state = steane_line_conn_phase_correction(phase_error_state)
-            corrected_state = ancilla_reset(corrected_state, self.n_ancilla)
-            self.assertTrue(np.allclose(initialized_zero_state, corrected_state))
-        # try 5 random bit flips (equal chance of any or no qubits)
-        for _ in range(5):
-            bit_error_state = bit_flip_error(initialized_zero_state, self.n_qtotal)[0]
-            corrected_state = steane_line_conn_bit_correction(bit_error_state)
-            corrected_state = ancilla_reset(corrected_state, self.n_ancilla)
-            self.assertTrue(np.allclose(initialized_zero_state, corrected_state))
 
 
 if __name__ == '__main__':
